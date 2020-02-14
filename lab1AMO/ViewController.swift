61693f230139781ff45e08bd6f543dc12c7f3a2a
Denis <danis.danilyuk@gmail.com>
2020-02-14 10:32:49 +0200

    Initial Commit

---
+++ lab1AMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab1AMO
+//
+//  Created by Денис Данилюк on 14.02.2020.
+//  Copyright © 2020 Денис Данилюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
